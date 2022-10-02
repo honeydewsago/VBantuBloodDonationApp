25e48322879d0ad7e53c725c8830fa2b004e242e
Jia Wei <jiawei392@gmail.com>
2022-10-02 19:42:03 +0800

    Connecting login to home

--- app/src/main/java/com/example/vbantublooddonationapp/LoginFragment.java
+++ app/src/main/java/com/example/vbantublooddonationapp/LoginFragment.java
@@ -1,5 +1,6 @@
 package com.example.vbantublooddonationapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,12 +9,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.vbantublooddonationapp.databinding.FragmentLoginBinding;
+import com.example.vbantublooddonationapp.databinding.FragmentRegisterBinding;
+
 public class LoginFragment extends Fragment {
+
+    private FragmentLoginBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        binding = FragmentLoginBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+
+        binding.flBtnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(),HomeActivity.class);
+                startActivity(i);
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        return view;
+        //return inflater.inflate(R.layout.fragment_login, container, false);
     }
 }
