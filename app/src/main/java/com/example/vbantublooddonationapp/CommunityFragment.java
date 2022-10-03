719c7b5a0049027a8670d08a4bea60fd78060abc
Wan <p20012339@student.newinti.edu.my>
2022-10-03 23:24:28 +0800

    Community XML

--- app/src/main/java/com/example/vbantublooddonationapp/CommunityFragment.java
+++ app/src/main/java/com/example/vbantublooddonationapp/CommunityFragment.java
@@ -7,14 +7,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toolbar;
+
+import com.example.vbantublooddonationapp.databinding.FragmentCommunityBinding;
 
 
 public class CommunityFragment extends Fragment {
+    FragmentCommunityBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_community, container, false);
+
+        binding = FragmentCommunityBinding.inflate(inflater,container,false);
+
+        return binding.getRoot();
+
     }
+
+
+
 }
