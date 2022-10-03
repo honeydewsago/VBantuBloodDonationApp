a3797c074e0a567864ee79764201b038183acdc4
Wan <p20012339@student.newinti.edu.my>
2022-10-04 01:02:07 +0800

    Community New Post XML

--- app/src/main/java/com/example/vbantublooddonationapp/CommunityFragment.java
+++ app/src/main/java/com/example/vbantublooddonationapp/CommunityFragment.java
@@ -7,7 +7,6 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toolbar;
 
 import com.example.vbantublooddonationapp.databinding.FragmentCommunityBinding;
 
