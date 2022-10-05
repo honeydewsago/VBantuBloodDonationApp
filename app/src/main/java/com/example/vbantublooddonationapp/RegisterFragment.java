f24c9955a00357356e5742e9bab3dfba4e532e4e
Jia Wei <jiawei392@gmail.com>
2022-10-06 00:57:34 +0800

    Rename strings resources

--- app/src/main/java/com/example/vbantublooddonationapp/RegisterFragment.java
+++ app/src/main/java/com/example/vbantublooddonationapp/RegisterFragment.java
@@ -32,11 +32,11 @@ public class RegisterFragment extends Fragment {
             public void onButtonChecked(MaterialButtonToggleGroup group, int checkedId, boolean isChecked) {
                 if (isChecked){
                     if (checkedId == R.id.fr_btnDonor) {
-                        binding.frEtFullName.setHint(R.string.Fullname);
+                        binding.frEtFullName.setHint(R.string.fullname);
                         userType = "donor";
                     }
                     else if (checkedId == R.id.fr_btnOrganiser){
-                        binding.frEtFullName.setHint(R.string.CompanyName);
+                        binding.frEtFullName.setHint(R.string.companyName);
                         userType = "organiser";
                     }
                 }
