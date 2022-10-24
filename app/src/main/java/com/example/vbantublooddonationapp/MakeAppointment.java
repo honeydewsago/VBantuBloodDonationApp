88b3d0f8e054a858152c6c4b3476f77663a157a5
Zyteoh <zyteoh0705@gmail.com>
2022-10-25 02:32:47 +0800

    Blood Appointment form design

---
+++ app/src/main/java/com/example/vbantublooddonationapp/MakeAppointment.java
@@ -0,0 +1,18 @@
+package com.example.vbantublooddonationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MakeAppointment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_appointment);
+    }
+
+    public void openDatePicker(View view) {
+    }
+}
