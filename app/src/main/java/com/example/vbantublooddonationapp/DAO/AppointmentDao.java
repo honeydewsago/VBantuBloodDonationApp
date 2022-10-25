012ffe64be4be58fb0cbe4eba9be48ec63fa3ea3
Zyteoh <zyteoh0705@gmail.com>
2022-10-26 00:14:01 +0800

    Blood Appointment form function

--- app/src/main/java/com/example/vbantublooddonationapp/DAO/AppointmentDao.java
+++ app/src/main/java/com/example/vbantublooddonationapp/DAO/AppointmentDao.java
@@ -1,7 +1,13 @@
 package com.example.vbantublooddonationapp.DAO;
 
 import androidx.room.Dao;
+import androidx.room.Insert;
+
+import com.example.vbantublooddonationapp.Model.Appointment;
 
 @Dao
 public interface AppointmentDao {
+
+    @Insert
+    void insert(Appointment...appointment);
 }
