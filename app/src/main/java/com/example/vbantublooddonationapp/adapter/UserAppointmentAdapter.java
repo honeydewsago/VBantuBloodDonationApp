e2a6b4ec7a063ee876f042be646395b94f5d0d20
Jia Wei <jiawei392@gmail.com>
2022-11-06 03:22:21 +0800

    Update database and user appointment adapter

--- app/src/main/java/com/example/vbantublooddonationapp/adapter/UserAppointmentAdapter.java
+++ app/src/main/java/com/example/vbantublooddonationapp/adapter/UserAppointmentAdapter.java
@@ -1,6 +1,8 @@
 package com.example.vbantublooddonationapp.adapter;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -12,8 +14,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.vbantublooddonationapp.Model.Appointment;
 import com.example.vbantublooddonationapp.Model.BloodRequest;
+import com.example.vbantublooddonationapp.Model.Organiser;
+import com.example.vbantublooddonationapp.Model.User;
+import com.example.vbantublooddonationapp.OrganiserSingleAppointmentDetails;
 import com.example.vbantublooddonationapp.R;
+import com.example.vbantublooddonationapp.SingleBloodBankLocation;
 import com.example.vbantublooddonationapp.ViewModel.BloodRequestViewModel;
+import com.example.vbantublooddonationapp.ViewModel.UserViewModel;
 import com.example.vbantublooddonationapp.databinding.CardBloodRequestHistoryBinding;
 import com.example.vbantublooddonationapp.databinding.CardUserAppointmentsBinding;
 
@@ -23,9 +30,11 @@ public class UserAppointmentAdapter extends RecyclerView.Adapter<UserAppointment
 
     private Activity mActivity;
     private List<Appointment> mAppointmentList;
+    private UserViewModel mUserViewModel;
 
     public UserAppointmentAdapter(Activity activity) {
         mActivity = activity;
+        mUserViewModel = new ViewModelProvider((FragmentActivity)mActivity).get(UserViewModel.class);
     }
 
     public void setAppointmentList(List<Appointment> appointmentList) {
@@ -42,9 +51,22 @@ public class UserAppointmentAdapter extends RecyclerView.Adapter<UserAppointment
     @Override
     public void onBindViewHolder(@NonNull AppointmentHolder holder, int position) {
         Appointment appointment = mAppointmentList.get(position);
-        holder.mTvName.setText("Halo");
-        holder.mTvDate.setText("12345656");
-        holder.mTvTime.setText("weeee");
+        String status = appointment.getStatus();
+        holder.mTvStatus.setText(status);
+
+        if (status.equals("Ongoing")) {
+            holder.mTvStatus.setBackgroundResource(R.color.orange);
+        }
+        else if (status.equals("Completed")) {
+            holder.mTvStatus.setBackgroundResource(R.color.green);
+        }
+        else{
+            holder.mTvStatus.setBackgroundResource(R.color.light_grey);
+        }
+
+        holder.mTvName.setText(getUserFullname(appointment.getUserID()));
+        holder.mTvDate.setText(getFullDate(appointment.getAppointmentDate()));
+        holder.mTvTime.setText(appointment.appointmentTime);
     }
 
     @Override
@@ -60,6 +82,8 @@ public class UserAppointmentAdapter extends RecyclerView.Adapter<UserAppointment
         private TextView mTvBloodType;
         private TextView mTvDate;
         private TextView mTvTime;
+        private TextView mTvStatus;
+        private TextView mTvDetails;
 
         public AppointmentHolder(CardUserAppointmentsBinding itemBinding) {
             super(itemBinding.getRoot());
@@ -67,6 +91,76 @@ public class UserAppointmentAdapter extends RecyclerView.Adapter<UserAppointment
             mTvBloodType = itemBinding.cuaTvBloodGroup;
             mTvDate = itemBinding.cuaTvAppointmentDate;
             mTvTime = itemBinding.cuaTvAppointmentTime;
+            mTvStatus = itemBinding.cuaTvStatus;
+            mTvDetails = itemBinding.cuaTvAppointmentDetails;
+
+            itemBinding.getRoot().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startSingleAppointmentActivity(getAdapterPosition());
+                }
+            });
+            mTvDetails.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startSingleAppointmentActivity(getAdapterPosition());
+                }
+            });
+        }
+    }
+
+    public void startSingleAppointmentActivity(int position){
+        Appointment currentAppointment = mAppointmentList.get(position);
+
+        Intent i = new Intent(mActivity, OrganiserSingleAppointmentDetails.class);
+
+        i.putExtra("currentAppointmentID", currentAppointment.getAppointmentID());
+
+        mActivity.startActivity(i);
+    }
+
+    private String getUserFullname(int id) {
+        List<User> userList = mUserViewModel.getUserById(id);
+        User user = userList.get(0);
+        return user.getFullName();
+    }
+
+    public String getFullDate(String dateTime) {
+        String year = dateTime.substring(0,4);
+        int month = Integer.parseInt(dateTime.substring(4,6));
+        String day = dateTime.substring(6,8);
+
+        return day + " "+ getMonthName(month) + " " + year;
+    }
+
+    public String getMonthName(int month_value){
+        switch (month_value) {
+            case 1:
+                return mActivity.getResources().getString(R.string.january);
+            case 2:
+                return mActivity.getResources().getString(R.string.february);
+            case 3:
+                return mActivity.getResources().getString(R.string.march);
+            case 4:
+                return mActivity.getResources().getString(R.string.april);
+            case 5:
+                return mActivity.getResources().getString(R.string.may);
+            case 6:
+                return mActivity.getResources().getString(R.string.june);
+            case 7:
+                return mActivity.getResources().getString(R.string.july);
+            case 8:
+                return mActivity.getResources().getString(R.string.august);
+            case 9:
+                return mActivity.getResources().getString(R.string.september);
+            case 10:
+                return mActivity.getResources().getString(R.string.october);
+            case 11:
+                return mActivity.getResources().getString(R.string.november);
+            case 12:
+                return mActivity.getResources().getString(R.string.december);
+            default:
+                return mActivity.getResources().getString(R.string.month);
         }
     }
 }
