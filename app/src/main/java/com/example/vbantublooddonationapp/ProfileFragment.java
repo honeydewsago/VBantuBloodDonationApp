818f36bc5380e78b918dd7ce3b594102eaabcf6c
Endairion <86313950+Endairion@users.noreply.github.com>
2022-11-07 22:34:08 +0800

    Fragment Profile now shows correct information for both personal and organiser.

--- app/src/main/java/com/example/vbantublooddonationapp/ProfileFragment.java
+++ app/src/main/java/com/example/vbantublooddonationapp/ProfileFragment.java
@@ -17,6 +17,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.vbantublooddonationapp.Model.Organiser;
 import com.example.vbantublooddonationapp.Model.User;
 import com.example.vbantublooddonationapp.ViewModel.OrganiserViewModel;
 import com.example.vbantublooddonationapp.ViewModel.UserViewModel;
@@ -30,6 +31,7 @@ public class ProfileFragment extends Fragment {
     private int mUserID = 1;
     private String mUserType = "user";
     private List<User> mUserList;
+    private List<Organiser> mOrganiserList;
 
     private UserViewModel mUserViewModel;
     private OrganiserViewModel mOrganiserViewModel;
@@ -49,7 +51,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        initUserViewModel();
+        initViewModel();
 
         toolbar = (Toolbar)getView().findViewById(R.id.fp_toolbar);
         AppCompatActivity activity = (AppCompatActivity)getActivity();
@@ -72,24 +74,43 @@ public class ProfileFragment extends Fragment {
             mUserType = mPreferences.getString(USERTYPE_KEY, "user");
         }
 
-        List<User> mUserList = mUserViewModel.getUserById(mUserID);
 
-        User user = mUserList.get(0);
 
-        profileBinding.fpTvUsername.setText(user.username);
-        profileBinding.fpTvEmail.setText(user.email);
-        profileBinding.fpTvIC.setText(user.icNo);
-        profileBinding.fpTvDOB.setText(user.dateOfBirth);
-        profileBinding.fpTvBloodGroup.setText(user.bloodType);
+
+
 
         if (mUserType.equals("organiser")) {
+            List<Organiser> mOrganiserList = mOrganiserViewModel.getOrganiserById(mUserID);
+            Organiser organiser = mOrganiserList.get(0);
+            profileBinding.fpTvUsername.setText(organiser.getPicName());
+            profileBinding.fpTvUserType.setText(R.string.user_type_o);
+            profileBinding.fpTvInfoType.setText(R.string.info_type_o);
+            profileBinding.fpTvCompany.setText(organiser.getCompanyName());
+            profileBinding.fpTvContactNo.setText(organiser.getContact());
+            profileBinding.fpTvAddress.setText(organiser.getAddress());
+
+            profileBinding.fpClPersonal.setVisibility(View.GONE);
+            profileBinding.fpClOrganiser.setVisibility(View.VISIBLE);
             profileBinding.fpIvQrCode.setVisibility(View.GONE);
             profileBinding.fpTvAppointmentHistory.setVisibility(View.GONE);
             profileBinding.fpTvRewards.setVisibility(View.GONE);
             profileBinding.fpTvUserAppointments.setVisibility(View.VISIBLE);
         }
         else {
+            List<User> mUserList = mUserViewModel.getUserById(mUserID);
+            User user = mUserList.get(0);
+
+            profileBinding.fpTvUserType.setText(R.string.user_type_bd);
             profileBinding.fpTvUserAppointments.setVisibility(View.GONE);
+            profileBinding.fpTvUsername.setText(user.username);
+            profileBinding.fpTvInfoType.setText(R.string.info_type_p);
+            profileBinding.fpTvEmail.setText(user.email);
+            profileBinding.fpTvIC.setText(user.icNo);
+            profileBinding.fpTvDOB.setText(user.dateOfBirth);
+            profileBinding.fpTvBloodGroup.setText(user.bloodType);
+
+            profileBinding.fpClPersonal.setVisibility(View.VISIBLE);
+            profileBinding.fpClOrganiser.setVisibility(View.GONE);
         }
 
         profileBinding.fpIvQrCode.setOnClickListener(new View.OnClickListener() {
@@ -123,7 +144,7 @@ public class ProfileFragment extends Fragment {
 
 
 
-    private void initUserViewModel(){
+    private void initViewModel(){
         mUserViewModel = new ViewModelProvider(this).get(UserViewModel.class);
 
         final Observer<List<User>> userListObserver = new Observer<List<User>>() {
@@ -133,6 +154,16 @@ public class ProfileFragment extends Fragment {
             }
         };
         mUserViewModel.getAllUsers().observe(getViewLifecycleOwner(),userListObserver);
+
+        mOrganiserViewModel = new ViewModelProvider(this).get(OrganiserViewModel.class);
+
+        final Observer<List<Organiser>> organiserListObserver = new Observer<List<Organiser>>() {
+            @Override
+            public void onChanged(List<Organiser> organisers) {
+                mOrganiserList = organisers;
+            }
+        };
+        mOrganiserViewModel.getAllOrganisers().observe(getViewLifecycleOwner(),organiserListObserver);
     }
 
 }
